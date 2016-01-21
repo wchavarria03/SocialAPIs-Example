ef6bd61f2984cde2d1d847c71771170f0f68957b
Walter Chavarria <walter.chavarria@avantica.net>
2016-01-21 11:56:33 -0600

    Added node-express service

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+const hostname = '127.0.0.1';
+const port = 3001;
+
+app.get('/', function(req, res){
+	res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(port);
