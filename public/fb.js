3ca47d7c6053f0a60c97c7ec8d5a8d60fbaa8637
Walter Chavarria <walter.chavarria@avantica.net>
2016-01-21 14:20:53 -0600

    Added bigger facebook login button

--- public/fb.js
+++ public/fb.js
@@ -61,9 +61,10 @@
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=905722622880560";
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
+
 
   // Here we run a very simple test of the Graph API after login is
   // successful.  See statusChangeCallback() for when this call is made.
